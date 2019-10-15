a2455933b8846e1b95648bdac50a5e4bee38738a
Giulio Eulisse <giulio.eulisse@cern.ch>
2019-10-16 00:26:16 +0200

    Align to v5-09-51 (#14)

--- Run2DataModel/src/AliESDEvent.cxx
+++ Run2DataModel/src/AliESDEvent.cxx
@@ -571,8 +571,8 @@ void AliESDEvent::ResetStdContent()
   if(fMuonGlobalTracks)fMuonGlobalTracks->Clear("C");     // AU
   if(fPmdTracks)fPmdTracks->Delete();
   if(fTrdTracks)fTrdTracks->Delete();
-  if(fTrdTracklets)fTrdTracklets->Delete();
-  if(fV0s)fV0s->Delete();
+  if(fTrdTracklets)fTrdTracklets->Clear();
+  if(fV0s)fV0s->Clear();
   if(fCascades)fCascades->Delete();
   if(fKinks)fKinks->Delete();
   if(fCaloClusters)fCaloClusters->Delete();
