bc8a9fc7f71cd8dc93a0018f77343dfa5d219d9b
Anton Alkin <anton.alkin@cern.ch>
2019-10-24 19:47:13 +0200

    Support for commandline options in converter
    * Adds "-n ###" option to limit number of events read from esd file

--- Converter/src/Run3AODConverter.h
+++ Converter/src/Run3AODConverter.h
@@ -18,24 +18,18 @@ namespace arrow {
 namespace io {
 class OutputStream;
 }
-}
+} // namespace arrow
 
-namespace o2
-{
-namespace framework
-{
-namespace run2
-{
+namespace o2::framework::run2 {
 
 /// Helpers for the Run2 ESD to Run3 AOD conversion.
 struct Run3AODConverter {
   // Helper to return a callback which is able to conver a Run2 ESD file to an
   // Arrow Table which then gets streamed to an ostream.
-  static void convert(TTree *tESD, std::shared_ptr<arrow::io::OutputStream> s);
+  static void convert(TTree *tESD, std::shared_ptr<arrow::io::OutputStream> s,
+                      size_t nEvents);
 };
 
-} // namespace run2
-} // namespace framework
-} // namespace o2
+} // namespace o2::framework::run2
 
 #endif // o2_framework_run2_Run3AODConverter_H_INCLUDED
